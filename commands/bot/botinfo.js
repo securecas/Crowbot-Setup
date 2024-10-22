05a30d57cc05e466e8039102624a96557b24b958
Léo MARCHAL <marchalleovh@gmail.com>
2024-10-22 09:16:05 +0200

    Update botinfo.js

--- commands/bot/botinfo.js
+++ commands/bot/botinfo.js
@@ -18,14 +18,14 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
 
             embed.setTitle(`Information à Propos De : ${client.user.username}`)
-            embed.setURL('https://discord.gg/9ZfB8m5E88')
-            embed.setDescription('**Une description complète et détaillée du Bot** <@1225087364821684379>')
+            embed.setURL('marchallebest')
+            embed.setDescription('**Une description complète et détaillée du Bot** <@1297612473779621968>')
             embed.setColor(color)
             .setThumbnail(message.author.avatarURL({ dynamic:true }));
             embed.setTimestamp()
             embed.setFooter(`${client.config.name}`)
             embed.addFields(
-                { name: '👑 Owner / Developer :', value: '<@1208337813339373569>', inline: true },
+                { name: '👑 Owner / Developer :', value: '<@1181618171308953741>', inline: true },
                 { name: '🔌 Latence Ping Bot :', value: `\`${client.ws.ping}Ms\`` },
                 { name: '🚀 Total Server(s) :', value: `\`${client.guilds.cache.size}\``, inline: true },
                 { name: '👥 Total User(s) :', value: `\`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}\``, inline: true },
